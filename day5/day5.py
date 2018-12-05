616532612b8171135835b2b2d6b3a3344de82d4f
adberl <berlovan97@gmail.com>
2018-12-05 14:12:32 +0200

    day 5

---
+++ day5/day5.py
@@ -0,0 +1,34 @@
+import re
+import numpy as np
+
+gpolymer = open('input.txt', 'r').readline()
+gpolymer = gpolymer[:-1] #get rid of '\n'
+
+min_polymer = len(gpolymer)
+
+def reactpolymer(polymer):
+	did_react = 1
+
+	while did_react:
+		did_react = 0
+		i = 0
+		
+		while (i < len(polymer) - 1):
+			current_unit = ord(polymer[i])
+			next_unit = ord(polymer[i+1])
+			
+			if(abs(current_unit - next_unit) == 32):
+				polymer = polymer[:i] + polymer[i+2:]
+				did_react=1
+				
+			i += 1
+	return len(polymer)		
+
+for i in range(0, 25):
+	
+	current = reactpolymer(gpolymer.replace(chr(65+i), '').replace(chr(97+i), ''))
+	min_polymer = current if current < min_polymer else min_polymer
+	
+print(min_polymer)
+
+
