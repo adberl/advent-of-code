215976423dac378791d6b6c089c50ea45759e7cb
hadber <adber@tuta.io>
2020-12-06 12:24:38 +0100

    day6

---
+++ 2020/day6/puzzle.py
@@ -0,0 +1,26 @@
+with open('input', 'r') as f:
+	lines = [i.strip() for i in f.readlines()]
+	
+	
+allgroups = []
+group_common = []
+current_group = set()	
+	
+for line in lines:
+	if line == '':
+		allgroups.append(set.intersection(*group_common))
+		current_group = set()
+		group_common = []
+	else:
+		for question in line:
+			current_group.add(question)
+		group_common.append(current_group)
+		current_group = set()
+		
+total_question = 0
+
+for group in allgroups:
+	total_question += len(group)
+
+print(total_question)
+		
