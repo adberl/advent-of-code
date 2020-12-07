5f21f4b41a325d7f451e1c001c9a0f06ad7a1770
hadber <adber@tuta.io>
2020-12-07 18:17:07 +0100

    day7 complete

--- 2020/day7/puzzle.py
+++ 2020/day7/puzzle.py
@@ -12,34 +12,48 @@ for line in lines:
 	containee_temp = []
 	for c in containee:
 		if(c[1] == '1'):
-			containee_temp.append(c[3:-4].rstrip())
+			containee_temp.append((1, c[3:-4].rstrip()))
 		else:
-			containee_temp.append(c[3:-5].rstrip())
+			containee_temp.append((int(c[1]), c[3:-5].rstrip()))
 	containee = containee_temp
 	
 	if (container in bag_graph):
 		bag_graph[container] += containee
 	else:
 		bag_graph[container] = containee
-	print(container, '\t', containee)
+#	print(container, '\t', containee)
 	
-def can_contain(current, path):
+def can_contain(bag):
 	ret = 0
-	print(current, path)
-	if current in path:
-		return ret
-	if not current in bag_graph:
-		return ret
-	else:
-		for a in bag_graph[current]:
-			if(a == 'shiny gold'):
-				ret += 1
+	if bag in bag_graph:
+#		print(f'{bag} has children {bag_graph[bag]}')
+		for child in bag_graph[bag]:
+			if child[1] == 'shiny gold':
+				return 1
 			else:
-				path.append(a)
-				ret += can_contain(a, path)
-	return ret	
+				ret += can_contain(child[1])
+	return 1 if ret >= 1 else 0
 
-total = 0
-for bag in bag_graph:
-	total += can_contain(bag, [])
-print(total)
+def partone():
+	total = 0
+
+	for bag in bag_graph:
+	#	print(f'looking for {bag}')
+		can = can_contain(bag)
+		total += can
+	return total
+	
+def count_children(bag):
+	total = 0
+#	print(f'counting children for {bag}')
+	if bag in bag_graph:
+		for child in bag_graph[bag]:
+			total += child[0] + child[0] * count_children(child[1]) if (child[1] in bag_graph) else child[0]
+#			print(total, child[0], child[1])
+	return total
+	
+def parttwo():
+	return count_children('shiny gold')
+	
+print('part one answer: ', partone())
+print('part two answer: ', parttwo())
