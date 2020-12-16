4cbd55c3d28e9a09e247fc5ae83a1a1e031f41bb
hadber <adber@tuta.io>
2020-12-16 19:54:38 +0100

    day13 p1

--- 2020/day13/puzzle.py
+++ 2020/day13/puzzle.py
@@ -15,3 +15,25 @@ def partone():
 		nearest = ceil(time / busid)
 		
 		print(busid, time - nearest*busid)	
+		
+		
+def parttwo():
+	ids = lines[1].split(',')
+	working = True
+	timestamp = 150000000000000 # if you start it at 0, it will run for an hour with no result
+	relative_timestamp = 0
+	busids = []
+	for idx, busid in enumerate(ids):
+		if(busid != 'x'):
+			busids.append((int(busid), idx))
+	while working:
+		timestamp += 1
+		for busid in busids:
+			if ((timestamp + busid[1]) % busid[0]) == 0:
+				if busid == busids[-1]:
+					print(timestamp)
+					working = False
+			else:
+				break
+
+parttwo()
