46d6bf54df81099b36b8594893e851932e6d7e81
hadber <adber@tuta.io>
2022-12-16 17:56:55 +0100

    leftover 2021

---
+++ 2021/day18/puzzle.py
@@ -0,0 +1,20 @@
+lines = [i.rstrip() for i in open('input', 'r').readlines()]
+
+sfnumbers = []
+
+class SFNumber():
+	def __init__(self, level, x, y)
+		self.level = level
+		self.x = x
+		self.y = y
+
+	def reduce(self):
+		pass
+
+	def explode(self):
+		pass
+
+	@classmethod
+	def add(cls, op1, op2)
+
+for line in 
