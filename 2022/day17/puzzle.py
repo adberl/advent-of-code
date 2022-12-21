63df73fff45aa8836f142e5739e6acfcf631c2bd
hadber <adber@tuta.io>
2022-12-22 00:08:14 +0100

    caching v0.2 probably abandoned

--- 2022/day17/puzzle.py
+++ 2022/day17/puzzle.py
@@ -33,23 +33,23 @@ def print_dummy_board(b, rock, current_row):
     print_board(dummy_board)
     print()
 
-def get_board_top():
+def get_board_top(x):
     for i in range(len(board)-1, 0, -1):
         if '#' in board[i]:
-            return board[i]
+            return board[i-x:i]
 
 cache = []
 
 placed_rocks = 0
 current_top = 1 # 0th element is the floor :)
 jetid = 0
-ROCK_LIMIT = 10000
+ROCK_LIMIT = 100000
 while placed_rocks < ROCK_LIMIT:
     for rock in tetrominos:
         #print(f'Currently working on {rock}')
 
-        if (rock, get_board_top()) in cache:
-            print(f'We have a cache hit!!!!! Turn: {placed_rocks}')
+        if (rock, get_board_top(8), jetid) in cache:
+           print(f'We have a cache hit!!!!! Turn: {placed_rocks}')
 
         if placed_rocks >= ROCK_LIMIT:
             break
@@ -128,7 +128,7 @@ while placed_rocks < ROCK_LIMIT:
         #print(f'current row is {current_row}')
         current_top = max(current_top, current_row + len(rock))
 
-        cache.append((rock, get_board_top()))
+        cache.append((rock, get_board_top(8), jetid))
 
         #print_dummy_board(board, rock, current_row)
 
