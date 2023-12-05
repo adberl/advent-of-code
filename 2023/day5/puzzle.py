349c41d159479aac53f57e8bd08de32b640ee022
hadber <adber@tuta.io>
2023-12-05 17:43:19 +0100

    funny ranges

--- 2023/day5/puzzle.py
+++ 2023/day5/puzzle.py
@@ -38,18 +38,17 @@ for mappings in all_maps:
 print(min(current_seeds))
 
 # test for part 2
-big_seeds = []
-for a, b in seed_ranges:
-    big_seeds += range(a, b+1)
+#big_seeds = []
+#for a, b in seed_ranges:
+#    big_seeds += range(a, b+1)
     
-for mappings in all_maps:
-    for i in range(len(big_seeds)):
-        for dest, src, arange in mappings:
-            delta = dest-src
-            if src <= big_seeds[i] <= src+arange:
-#                print(f"Transforming {current_seeds[i]} into {current_seeds[i]+delta}")
-                big_seeds[i] += delta
-                break
+#for mappings in all_maps:
+#    for i in range(len(big_seeds)):
+#        for dest, src, arange in mappings:
+#            delta = dest-src
+#            if src <= big_seeds[i] <= src+arange:
+#                big_seeds[i] += delta
+#                break
  #   print(sorted(big_seeds))
 #print(big_seeds)
 #part 2
@@ -62,12 +61,12 @@ for mappings in all_maps:
             delta = dest-src 
             if i >= len(seed_ranges):
                 break
-            print(f"Starting {i}", seed_ranges)
+            #print(f"Starting {i}", seed_ranges)
             start, end = seed_ranges[i]
         
             # simplest case, the entire range can be mapped
             if src <= start <= src+arange and src <= end <= src+arange:
-                print("entire", delta, start, end, src, dest)
+                #print("entire", delta, start, end, src, dest)
                 seed_ranges[i] = (start+delta, end+delta)
                 i += 1
                 changed = True
@@ -76,8 +75,8 @@ for mappings in all_maps:
             elif src <= start <= src+arange and not (src <= end <= src+arange):
                 # figure out how many seeds are contained in the range
                 contained = (src+arange) - start
-                print("start", contained, delta, start, end, src, dest)
-                print("two new: ", (start+delta, start+delta+contained), (start+contained+1, end))
+                #print("start", contained, delta, start, end, src, dest)
+                #print("two new: ", (start+delta, start+delta+contained), (start+contained+1, end))
                 seed_ranges[i] = (start+delta, start+delta+contained)
                 seed_ranges.insert(i+1, (start+contained+1, end))
                 print(seed_ranges)
@@ -87,29 +86,30 @@ for mappings in all_maps:
             # if range includes end
             elif not (src <= start <= src+arange) and src <= end <= src+arange:
                 contained = end - src
-                print("end", contained, delta, start, end, src, dest, arange)
-                print("two new: ", (src+delta, end+delta), (start, src-1))
+                #print("end", contained, delta, start, end, src, dest, arange)
+                #print("two new: ", (src+delta, end+delta), (start, src-1))
                 seed_ranges.insert(i+1, (start, src-1))
                 seed_ranges[i] = (src+delta, end+delta)
-                print(seed_ranges)
+                #print(seed_ranges)
                 i += 1
                 changed = True
             
             # if mapping range entirely contained
             elif start <= src and src+arange <= end:
-                print("mid", delta, start, end, src, dest)
-                seed_ranges[i] = (start, src-1)
+                #print("mid", delta, start, end, src, dest)
+                seed_ranges[i] = (src+arange+1, end)#(start, src-1)
+                seed_ranges.insert(i+1, (start, src-1))
                 seed_ranges.insert(i+2, (src, src+arange)) 
-                seed_ranges.insert(i+1, (src+arange+1, end))
-                i += 2
+                i += 1
                 changed = True
         
         if not changed:
             i += 1
         #print(seed_ranges)
         #i += 1
-    print('Seed ranges after mapping: ', seed_ranges)
-    print(mappings)
-    print()
+    #print('Seed ranges after mapping: ', seed_ranges)
+    #print(mappings)
+    #print()
 
-print(seed_ranges)
+
+print(min(seed_ranges)[0])
